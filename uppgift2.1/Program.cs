5964bc47a13b77d2c3bf2ca17b998c3bd409e79b
emsj06001 <emsj06001@E-PW024NWP>
2023-09-15 12:24:27 +0200

    Add project files.

---
+++ uppgift2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppgift_2_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favoritbok?...");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Din favorit bok är "+ bok +".");
+            Console.ReadKey();
+        }
+    }
+}
